6db56d4b2b0ee44326019c3d5aa693d37655a9ef
Brandon Bergenroth <bbergenroth@rti.org>
2021-08-09 16:10:16 -0400

    add basic type support, naming, nodata values

--- main.go
+++ main.go
@@ -5,12 +5,49 @@ import (
 	"fmt"
 	"github.com/xuri/excelize/v2"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func clean(s string) string {
+	var replacer = strings.NewReplacer(" ", "_", "\n", "_", "(", "", ")", "")
+	return strings.ToLower(replacer.Replace(s))
+}
+
+func isNumber(t string) bool {
+	_, err := strconv.ParseFloat(t, 64)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func toCut(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+type stripFlags []string
+
+func (i *stripFlags) String() string {
+	return "nodata"
+}
+
+func (i *stripFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+var stripFlag stripFlags
 
 func main() {
 	w := flag.String("w", "", "optional worksheet name (defaults to first)")
 	s := flag.Uint("s", 0, "optional rows to skip")
+	t := flag.String("t", "", "optional table name (defaults to sheet name")
+	flag.Var(&stripFlag, "c", "nodata values to convert to null")
 	flag.Parse()
 	flag.Usage = func() {
 		fmt.Fprint(os.Stderr, "Usage: xls2csv [-w worksheet] [-s lines] filename\n")
@@ -23,6 +60,7 @@ func main() {
 	}
 
 	var worksheet string
+	var tablename string
 
 	if *w != "" {
 		worksheet = *w
@@ -35,6 +73,12 @@ func main() {
 	}
 	if worksheet == "" {
 		worksheet = f.GetSheetName(0)
+	}
+
+	if *t != "" {
+		tablename = *t
+	} else {
+		tablename = worksheet
 	}
 
 	rows, err := f.Rows(worksheet)
@@ -54,21 +98,22 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		var sql = "insert into t values ("
+		var sql = "insert into " + tablename + "  values ("
 		for n, col := range row {
 			if i == *s { //header
-				//TODO create func to strip all non-allowable chars
-				col = strings.ReplaceAll(col, " ", "_")
-				col = strings.ReplaceAll(col, "\n", "_")
-				col = strings.ReplaceAll(col, "(", "")
-				col = strings.ReplaceAll(col, ")", "")
-				colnames = append(colnames, col)
-				coltypes = append(coltypes, "text")
+				colnames = append(colnames, clean(col))
+				coltypes = append(coltypes, "text") //default type
 			} else if i > *s {
-					//TODO check if cell can be converted to number or date
-					sql = sql + "\"" + col + "\","
-					//TODO set type here
-					coltypes[n] = "numeric"
+					//TODO check if cell can be converted to date
+					if isNumber(col) {
+						//TODO add db dialects (postgreSQL, Oracle, etc)
+						coltypes[n] = "numeric"
+						sql = sql + col + ","
+					} else if col == "" || toCut(col, stripFlag){
+						sql = sql + "NULL,"
+					} else {
+						sql = sql + "\"" + col + "\","
+					}
 			}
 		}
 		if i > *s {
@@ -80,7 +125,7 @@ func main() {
 		}
 	}
 
-	fmt.Print("create table t (")
+	fmt.Print("create table " + clean(tablename) + " (")
 	for n, c := range colnames {
 		fmt.Print(c, " ", coltypes[n])
 		if n < len(colnames) - 1 {
